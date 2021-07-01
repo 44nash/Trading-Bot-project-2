7cf260edbfecb197c91c52f57a908f6a2fcea62c
Nathan S. Froemming <natfro@gmail.com>
2021-07-01 13:00:24 -0700

    Helper function to calculate the exponential moving average (EMA) of an input timeseries

--- technical_analysis.py
+++ technical_analysis.py
@@ -10,6 +10,21 @@ def get_sma(timeseries: Union[pd.Series, pd.DataFrame], window: int) -> Union[pd
     return timeseries.rolling(window=window, min_periods=window).mean()
 
 
+def get_ema(timeseries: Union[pd.Series, pd.DataFrame] = None,
+            window: float = None,
+            calc_method: str = 'span') -> Union[pd.Series, pd.DataFrame]:
+    """
+    Helper function to calculate the exponential moving average (EMA) of an
+    input timeseries.
+
+    See [here](https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.ewm.html?highlight=ewm#pandas.DataFrame.ewm)
+    for information about the four `calc_methods` {'alpha', 'com', 'halflife',
+    'span'}.
+    """
+    kwargs = {calc_method.lower(): window, 'min_periods': window}
+    return timeseries.ewm(**kwargs).mean()
+
+
 def main() -> None:
     return None
 
