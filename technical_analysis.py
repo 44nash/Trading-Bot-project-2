518225155b74e5ed9c0642bdfb6cd2ea0609c9bf
Nathan S. Froemming <natfro@gmail.com>
2021-07-01 11:36:58 -0700

    Adding helper function to calculate the simple moving average (SMA) of an input timeseries

--- technical_analysis.py
+++ technical_analysis.py
@@ -1,3 +1,15 @@
+import pandas as pd
+from typing import Union
+
+
+def get_sma(timeseries: Union[pd.Series, pd.DataFrame], window: int) -> Union[pd.Series, pd.DataFrame]:
+    """
+    Helper function to calculate the simple moving average (SMA) of an input
+    timeseries.
+    """
+    return timeseries.rolling(window=window, min_periods=window).mean()
+
+
 def main() -> None:
     return None
 
